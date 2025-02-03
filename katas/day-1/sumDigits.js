df5bb07dcb2f4072b9ab964bea1febcca515a9c4
ssh1992 <simsonhannawell@gmail.com>
2025-02-03 12:22:23 +0000

    sumdigits

--- katas/day-1/sumDigits.js
+++ katas/day-1/sumDigits.js
@@ -1,3 +1,11 @@
-function sumDigits() {}
+function sumDigits(numbers) {
+    // Convert number to a string, then split it into an array of digits
+    let digitsArray = numbers.toString().split("").map(Number);
+
+    // Sum up the digits
+    let sum = digitsArray.reduce((acc, digit) => acc + digit, 0);
+
+    return sum;
+}
 
 module.exports = sumDigits;
