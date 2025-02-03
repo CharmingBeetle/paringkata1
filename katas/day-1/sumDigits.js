33ac21b3d510610060df2bfebb05cee59ce33ac6
CharmingBeetle <nina.e.thomas@gmail.com>
2025-02-03 16:29:30 +0000

    Completed and passed tests on Sum digits

--- katas/day-1/sumDigits.js
+++ katas/day-1/sumDigits.js
@@ -1,7 +1,8 @@
 function sumDigits(numbers) {
     // Convert number to a string, then split it into an array of digits
-    let digitsArray = numbers.toString().split("").map(Number);
-
+    let numStr = numbers.toString()
+    numStr = numStr.replace(".", "")
+    let digitsArray = numStr.split("").map(Number)
     // Sum up the digits
     let sum = digitsArray.reduce((acc, digit) => acc + digit, 0);
 
