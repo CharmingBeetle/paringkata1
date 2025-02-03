e4ddb503dd75a8ac231a78319d01af5fc80ae2a7
CharmingBeetle <nina.e.thomas@gmail.com>
2025-02-03 16:32:14 +0000

    Completed and passed tests on Count Veg

--- katas/day-1/countVeg.js
+++ katas/day-1/countVeg.js
@@ -1,3 +1,38 @@
-function countVeg() {}
+// const vegetables = [
+//   {name: 'Parsnip', type: 'root', quantity: 4},
+//   {name: 'Broccoli', type: 'brassica', quantity: 1},
+//   {name: 'Carrot', type: 'root', quantity: 5},
+//   {name: 'Onion', type: 'bulb', quantity: 3},
+//   {name: 'Chard', type: 'leaf', quantity: 3},
+//   {name: 'Runner beans', type: 'legume', quantity: 8}
+// ]
+
+
+function countVeg(vegetables, vegType) {
+    let total = 0 
+    for(let i=0; i < vegetables.length; i++) {
+        const vegObj = vegetables[i]
+        if(vegObj.type === vegType) {
+            total += vegObj.quantity 
+        }
+    }
+    return total
+}
+
 
 module.exports = countVeg;
+
+
+// countVeg([{name: 'Runner beans', type: 'legume', quantity: 4}], 'legume')
+// // => 4
+
+// const vegetables = [
+//   {name: 'Parsnip', type: 'root', quantity: 4},
+//   {name: 'Broccoli', type: 'brassica', quantity: 1},
+//   {name: 'Carrot', type: 'root', quantity: 5},
+//   {name: 'Onion', type: 'bulb', quantity: 3},
+//   {name: 'Chard', type: 'leaf', quantity: 3},
+//   {name: 'Runner beans', type: 'legume', quantity: 8}
+// ]
+// countVeg(vegetables, 'root')
+// // => 9
