7f9948ffb3e1f0463b97aaac4a98f1d4db6ca9fd
CharmingBeetle <nina.e.thomas@gmail.com>
2025-02-04 23:21:13 +0000

    Completed testing for organise wardrobe

--- __tests__/organiseWardrobe.test.js
+++ __tests__/organiseWardrobe.test.js
@@ -1,11 +1,53 @@
 const organiseWardrobe = require("../katas/day-2/organiseWardrobe.js") 
 
 describe("organiseWardrobe", () => {
-    test("returns one clothing item ", () => {
-        // Assign
 
+    test("returns empty array when no matches", () => {
+        // Assign
+        const clothes = [
+            { item: "ugly uggs", purchase_year: 2017, coolness_rating: 1 },
+            { item: "fluorescent yellow skinny jeans", purchase_year: 2005, coolness_rating: 10},
+            { item: "slouchy beanie", purchase_year: 2017, coolness_rating: -1 },
+            { item: "pristine sequined ballet flats", purchase_year: 2013, coolness_rating: 8},
+            { item: "floral print doc martens", purchase_year: 2014, coolness_rating: 6}
+        ]
+        const expected = []
         // Act
+        const actual = organiseWardrobe(clothes)
         // Assert
+        expect(actual).toEqual(expected)
+    })
+    
+    test("returns one clothing item when only one entry", () => {
+        // Assign
+        const clothes = [
+            { item: "ugly uggs", purchase_year: 2007, coolness_rating: 1 },
+        ]
+        const expected = [
+            { item: "ugly uggs", purchase_year: 2007, coolness_rating: 1, action: "sell" }
+        ]
+        // Act
+        const actual = organiseWardrobe(clothes)
+        // Assert
+        expect(actual).toEqual(expected)
+    })
+
+    test("returns one clothing item when one item fits criteria ", () => {
+        // Assign
+        const clothes = [
+            { item: "ugly uggs", purchase_year: 2017, coolness_rating: 1 },
+            { item: "fluorescent yellow skinny jeans", purchase_year: 2005, coolness_rating: 3},
+            { item: "slouchy beanie", purchase_year: 2017, coolness_rating: -1 },
+            { item: "pristine sequined ballet flats", purchase_year: 2013, coolness_rating: 8},
+            { item: "floral print doc martens", purchase_year: 2014, coolness_rating: 6}
+        ]
+        const expected = [
+            { item: "fluorescent yellow skinny jeans", purchase_year: 2005, coolness_rating: 3, action: "sell" }
+        ]
+        // Act
+        const actual = organiseWardrobe(clothes)
+        // Assert
+        expect(actual).toEqual(expected)
     })
 
 
